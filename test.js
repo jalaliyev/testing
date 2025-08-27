279b2a9fd99188296ddfb138ba2976fa66f0df59
Jalal Aliyev <jalalaliyev@Jalals-MacBook-Pro.local>
2025-08-27 20:36:47 +0400

    implemented subtract logic

--- test.js
+++ test.js
@@ -5,3 +5,11 @@ function sum(a, ...rest) {
 }
 
 console.log(sum(1, 2, 4, 5, 6));
+
+function subtract(a, ...rest) {
+  let total = a;
+  for (let i = 0; i < rest.length; i++) total -= rest[i];
+  return total;
+}
+
+console.log(subtract(100, 5, 6));
